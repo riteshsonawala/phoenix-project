d35bb1ba6de6d373af46229f4ab6558297a90f66
riteshsonawala <ritesh.sonawala@gmail.com>
2022-12-29 01:43:09 +0000

    Initial Add

---
+++ src/test/java/com/rs/phoenixproject/PhoenixProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.phoenixproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
